1689323259256c7296ec85d2d24257c622a6cf07
Manikhandan Mavureddy Senthilvelu <mavureddysenthilve.m@northeastern.edu>
2026-02-19 17:07:34 -0500

    added job for pr-check

--- jobs/static_site_job.groovy
+++ jobs/static_site_job.groovy
@@ -42,3 +42,48 @@ multibranchPipelineJob('static-site') {
         }
     }
 }
+
+// PR status check job - validates conventional commits on PRs
+multibranchPipelineJob('static-site-pr-check') {
+    displayName('Static Site - PR Status Check')
+    description('Validate conventional commits on pull requests')
+
+    branchSources {
+        branchSource {
+            source {
+                github {
+                    id('static-site-pr-check')
+                    repoOwner('__GITHUB_ORG__')
+                    repository('__GITHUB_REPO__')
+                    credentialsId('github-credentials')
+                    configuredByUrl(true)
+                    repositoryUrl('https://github.com/__GITHUB_ORG__/__GITHUB_REPO__.git')
+                    traits {
+                        gitHubBranchDiscovery {
+                            strategyId(1)
+                        }
+                        gitHubPullRequestDiscovery {
+                            strategyId(1)  // Merge PR head with target branch
+                        }
+                        headWildcardFilter {
+                            includes('PR-*')
+                            excludes('')
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(10)
+        }
+    }
+
+    factory {
+        workflowBranchProjectFactory {
+            scriptPath('Jenkinsfile.pr')
+        }
+    }
+}
